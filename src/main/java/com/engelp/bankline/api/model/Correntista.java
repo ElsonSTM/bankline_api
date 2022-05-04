56ea8c7c14dfb13e88a6eb271bb3870902311a8b
ElsonSTM <86759660+ElsonSTM@users.noreply.github.com>
2022-05-04 02:00:19 -0300

    Mapeado as entidades com JPA

--- src/main/java/com/engelp/bankline/api/model/Correntista.java
+++ src/main/java/com/engelp/bankline/api/model/Correntista.java
@@ -1,11 +1,22 @@
 package com.engelp.bankline.api.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(length = 20)
     private String cpf;
+
+    @Column(length = 60)
     private String nome;
 
+    @Embedded //relacionamneto entre classe Correntista c/ Conta
     private Conta conta;
 
     public Conta getConta() {

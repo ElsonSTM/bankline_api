32ecab213d5f9d21fc1402f0d0bca51455a16565
ElsonSTM <86759660+ElsonSTM@users.noreply.github.com>
2022-05-07 01:22:05 -0300

    Impl. do método de buscar por id do correntista e ajustes no model.

--- src/main/java/com/engelp/bankline/api/model/Movimentacao.java
+++ src/main/java/com/engelp/bankline/api/model/Movimentacao.java
@@ -1,5 +1,7 @@
 package com.engelp.bankline.api.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
@@ -11,6 +13,7 @@ public class Movimentacao {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     @Column(name = "data_hora")
     private LocalDateTime dataHora;
 
--- src/main/java/com/engelp/bankline/api/repository/MovimentacaoRepository.java
+++ src/main/java/com/engelp/bankline/api/repository/MovimentacaoRepository.java
@@ -3,5 +3,9 @@ package com.engelp.bankline.api.repository;
 import com.engelp.bankline.api.model.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
+
+    public List<Movimentacao> findByIdConta(Integer idConta);
 }
